d4bd386dc32a31a75a60d5801cb97c8dff573675
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2020-03-26 19:48:02 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -13,9 +13,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""main URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path
+from main1.views import TeamsView, TasksView, TeamsDetailView, AdminTeamView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('teams/', TeamsView.as_view(),
+         name='team_view'),
+    path('tasks/', TasksView.as_view(),
+         name='tasks_view'),
+    path('teams_detail', TeamsDetailView.as_view(),
+         name='teams_detail'),
+    path('admin_team', AdminTeamView.as_view(),
+         name='admin_team'),
 ]
