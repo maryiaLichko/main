c0876e71a6e5c4ae0fa12d621545a7247f27a83e
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2020-03-26 19:48:32 +0300

    Update admin.py

--- main1/admin.py
+++ main1/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from main1.models import Teams
+
+
+class TeamsAdmin(admin.TeamsAdmin):
+    model = Teams
+
+
+admin.site.register(TeamsAdmin)
