dcc30f2300632bcb6a22e8507b297038a8814391
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2020-03-26 19:47:25 +0300

    Views.py was updated

--- main1/views.py
+++ main1/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 # Create your views here.
+from main1.models import Teams, Tasks
+
+
+class TeamsView(ListView):
+    model = Teams
+
+
+class TasksView(ListView):
+    model = Tasks
+
+
+class TeamsDetailView(DetailView):
+    model = Teams
+
+
+class AdminTeamView(ListView):
+    model = Teams
