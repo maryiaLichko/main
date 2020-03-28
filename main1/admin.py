9a6777cb16d1aedaba5a73ced0e1ebdc4b98b644
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2020-03-28 21:41:14 +0300

    added admin TASKS

--- main1/admin.py
+++ main1/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from main1.models import Teams
+from main1.models import Teams, Tasks
 
 
 admin.site.register(Teams)
+admin.site.register(Tasks)
