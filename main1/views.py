0e8e040ca1b4bfc094c749a5fdd95a6503872f9a
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2020-03-30 16:11:02 +0300

    fixed

--- main1/views.py
+++ main1/views.py
@@ -22,5 +22,5 @@ class TeamsDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context['participants_list'] = Teams.get
+        context['participants_list'] = Teams.profile_set.all()
         return context
